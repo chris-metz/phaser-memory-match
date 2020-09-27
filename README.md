8c2c1db7736629d9d16d0a52124b02f158e56ff1
Chris Metz <me@chris-metz.dev>
2020-09-27 20:47:33 +0200

    01 - Creating a Character done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # phaser-memory-match
+
+Memory Match Phaser 3 demonstration game based on
+
+https://www.youtube.com/playlist?list=PLumYWZ2t7CRuhzvpSeCvRLUZv2xKgO_wh
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import Phaser from 'phaser';
+import Preloader from './scenes/Preloader';
+import Game from './scenes/Game';
+
+const config = {
+  width: 700,
+  height: 600,
+  type: Phaser.AUTO,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      // debug: true,
+    },
+  },
+  scale: {
+    parent: 'gameWrapper',
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  scene: [Preloader, Game],
+};
+
+export default new Phaser.Game(config);
