ba5f39ce35d3ae17693d20209d7b857030efe061
Chris Metz <me@chris-metz.dev>
2020-09-27 22:48:21 +0200

    02 - Adding Boxes with Depth Sorting done

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -7,19 +7,37 @@ export default class Preloader extends Phaser.Scene {
   /** @type {Phaser.Physics.Arcade.Sprite} */
   player;
 
+  /** @type {Phaser.Physics.Arcade.StaticGroup} */
+  boxGroup;
+
   constructor() {
     super('game');
   }
 
   init() {
-    this.cursors = this.input.keyboard.createCursorKeys();
+    // this.cursors = this.input.keyboard.createCursorKeys();
+    this.cursors = this.input.keyboard.addKeys({
+      up: Phaser.Input.Keyboard.KeyCodes.W,
+      down: Phaser.Input.Keyboard.KeyCodes.S,
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
+    });
   }
 
   create() {
     const { width, height } = this.scale;
     this.player = this.physics.add
       .sprite(width * 0.5, height * 0.5, 'sokoban')
+      .setSize(40, 16)
+      .setOffset(12, 38)
       .play('down-idle');
+
+    this.boxGroup = this.physics.add.staticGroup();
+    this.boxGroup.get(width * 0.25, 150, 'sokoban', 10);
+
+    this.createBoxes();
+
+    this.physics.add.collider(this.player, this.boxGroup);
   }
 
   update() {
@@ -43,6 +61,30 @@ export default class Preloader extends Phaser.Scene {
       const parts = key.split('-');
       const direction = parts[0];
       this.player.play(`${direction}-idle`);
+    }
+
+    // wouldnt have to be in update, as long as only the player moves
+    this.children.each(c => {
+      /** @type {Phaser.Physics.Arcade.Sprite} */
+      const child = c;
+      child.setDepth(child.y);
+    });
+  }
+
+  createBoxes() {
+    const { width } = this.scale;
+    let xPer = 0.25;
+    let y = 150;
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        /** @type {Phaser.Physics.Arcade.Sprite} */
+        const box = this.boxGroup.get(width * xPer, y, 'sokoban', 10);
+        box.setSize(64, 32);
+        box.setOffset(0, 32);
+        xPer += 0.25;
+      }
+      xPer = 0.25;
+      y += 150;
     }
   }
 }
